cce118639cccf81489757e4f636f1ac0089296fd
Dougal Matthews <dougal85@gmail.com>
2011-12-02 11:38:35 +0000

    Added second test form.

--- tests/test_formadmin/admin.py
+++ tests/test_formadmin/admin.py
@@ -1,10 +1,17 @@
 from formadmin.admin import FormAdmin
 from formadmin import sites
 
-from test_formadmin.forms import EmailForm
+from test_formadmin.forms import EmailForm, UploadForm
 
 
 class EmailFormAdmin(FormAdmin):
+    verbose_name = "Email Form"
+    verbose_name_plural = "Email Forms"
+
+
+class UploadFormAdmin(FormAdmin):
     pass
 
+
 sites.register(EmailForm, EmailFormAdmin)
+sites.register(UploadForm, UploadFormAdmin)
--- tests/test_formadmin/forms.py
+++ tests/test_formadmin/forms.py
@@ -4,3 +4,8 @@ from django import forms
 class EmailForm(forms.Form):
 
     email = forms.EmailField()
+
+
+class UploadForm(forms.Form):
+
+    file = forms.FileField()
